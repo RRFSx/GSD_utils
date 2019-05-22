7471096db5aa0228e01aa6bef123876703a88c28
Ming.Hu <Ming.Hu@noaa.gov>
2019-05-22 21:19:24 +0000

    Merge with branch "jetupgrade".

--- data_process/fvcom/module_nwp.f90
+++ data_process/fvcom/module_nwp.f90
@@ -22,29 +22,18 @@ module module_nwp
    private
    type :: nwp_type
       character(len=5) :: datatype
-      integer :: numvar, xlat, xlon, xtime, xvert
-      integer :: i_mask, i_sst, i_ice, i_sfcT, i_sltyp, i_vgtyp
-      integer :: i_land, i_lu, i_snow, i_snowh, i_snowc
+      integer :: numvar, xlat, xlon, xtime
+      integer :: i_mask, i_sst, i_ice, i_sfcT
       character(len=20), allocatable :: varnames(:)
-      character(len=20), allocatable :: varname3D
       character(len=20), allocatable :: latname
       character(len=20), allocatable :: lonname
       character(len=20), allocatable :: dimnameEW
       character(len=20), allocatable :: dimnameNS
       character(len=20), allocatable :: dimnameTIME
-      character(len=20), allocatable :: dimnameVERT
       real(r_single), allocatable :: nwp_mask(:,:,:)
       real(r_single), allocatable :: nwp_sst(:,:,:)
       real(r_single), allocatable :: nwp_ice(:,:,:)
       real(r_single), allocatable :: nwp_sfcT(:,:,:)
-      real(r_single), allocatable :: nwp_land(:,:,:)
-      real(r_single), allocatable :: nwp_lu(:,:,:)
-      real(r_single), allocatable :: nwp_snow(:,:,:)
-      real(r_single), allocatable :: nwp_snowh(:,:,:)
-      real(r_single), allocatable :: nwp_snowc(:,:,:)
-      integer, allocatable :: nwp_sltyp(:,:,:)
-      integer, allocatable :: nwp_vgtyp(:,:,:)
-      real(r_single), allocatable :: nwp_soilmoisture(:,:,:,:)
    end type nwp_type
 
    type, extends(nwp_type) :: fcst_nwp
@@ -54,7 +43,6 @@ module module_nwp
          procedure :: initial => initial_nwp
          procedure :: list_initial => list_initial_nwp
          procedure :: read_n => read_nwp
-         procedure :: read_n3 => read_nwp_3d
          procedure :: finish => finish_nwp
    end type fcst_nwp
 
@@ -83,13 +71,6 @@ module module_nwp
             this%i_sst = 2
             this%i_ice = 3
             this%i_sfcT = 0
-            this%i_sltyp = 0
-            this%i_vgtyp = 0
-            this%i_land = 0
-            this%i_lu = 0
-            this%i_snow = 0
-            this%i_snowh = 0
-            this%i_snowc = 0
 
             allocate(this%varnames(this%numvar))
             this%varnames(1) = 'LAKEMASK'
@@ -104,44 +85,26 @@ module module_nwp
             allocate(this%dimnameEW)
             allocate(this%dimnameNS)
             allocate(this%dimnameTIME)
-            allocate(this%dimnameVERT)
             this%dimnameEW = 'west_east'
             this%dimnameNS = 'south_north'
             this%dimnameTIME = 'time'
-            this%dimnameVERT = 'soil_layers_stag'
-
-            this%varname3D = 'SMOIS'
 
 !        HRRR grid
 
          else if (itype==' HRRR') then
             this%datatype = itype
-            this%numvar = 11
+            this%numvar = 4
 
             this%i_mask = 1
             this%i_sst = 2
             this%i_ice = 3
             this%i_sfcT = 4
-            this%i_sltyp = 5
-            this%i_vgtyp = 6
-            this%i_land = 7
-            this%i_lu = 8
-            this%i_snow = 9
-            this%i_snowh = 10
-            this%i_snowc = 11
 
             allocate(this%varnames(this%numvar))
             this%varnames(1) = 'LANDMASK'
             this%varnames(2) = 'SST'
             this%varnames(3) = 'SEAICE'
             this%varnames(4) = 'TSK'
-            this%varnames(5) = 'ISLTYP'
-            this%varnames(6) = 'IVGTYP'
-            this%varnames(7) = 'XLAND'
-            this%varnames(8) = 'LU_INDEX'
-            this%varnames(9) = 'SNOW'
-            this%varnames(10) = 'SNOWH'
-            this%varnames(11) = 'SNOWC'
 
             allocate(this%latname)
             allocate(this%lonname)
@@ -151,13 +114,9 @@ module module_nwp
             allocate(this%dimnameEW)
             allocate(this%dimnameNS)
             allocate(this%dimnameTIME)
-            allocate(this%dimnameVERT)
             this%dimnameEW = 'west_east'
             this%dimnameNS = 'south_north'
             this%dimnameTIME = 'Time'
-            this%dimnameVERT = 'soil_layers_stag'
-
-            this%varname3D = 'SMOIS'
 
 !        If the data type does not match one of the known types, exit.
 
@@ -185,11 +144,9 @@ module module_nwp
 
          write(*,*) 'List initial setup for ', this%datatype
          write(*,*) 'number of variables ', this%numvar
-         write(*,*) 'variable index: mask, sst, ice, sfcT, sltyp, vgtyp, land, &
-      &      lu, snow, snowh, snowc'
+         write(*,*) 'variable index: mask, sst, ice, sfcT'
          write(*,'(15x,10I3)') this%i_mask, this%i_sst, this%i_ice, &
-      &      this%i_sfcT, this%i_sltyp, this%i_vgtyp, this%i_land, this%i_lu, &
-      &      this%i_snow, this%i_snowh, this%i_snowc
+      &      this%i_sfcT
          write(*,*) 'variable name:'
          do k=1,this%numvar
             write(*,*) k,trim(this%varnames(k))
@@ -200,7 +157,7 @@ module module_nwp
 
       end subroutine list_initial_nwp
 
-      subroutine read_nwp(this,filename,itype,numlon,numlat,numtimes,time_to_get,mask,sst,ice,sfcT,sltyp,vgtyp,land,lu,snow,snowh,snowc)
+      subroutine read_nwp(this,filename,itype,numlon,numlat,numtimes,time_to_get,mask,sst,ice,sfcT)
 
 !        This subroutine initializes arrays to receive the NWP data,
 !        and opens the file and gets the data.
@@ -213,8 +170,6 @@ module module_nwp
          integer, intent(in) :: time_to_get
          integer, intent(inout) :: numlon, numlat, numtimes
          real(r_single), intent(inout) :: mask(:,:), sst(:,:), ice(:,:), sfcT(:,:)
-         real(r_single), intent(inout) :: land(:,:), lu(:,:), snow(:,:), snowh(:,:), snowc(:,:)
-         integer, intent(inout) :: sltyp(:,:), vgtyp(:,:)
 
 !        Open the file using module_ncio.f90 code, and find the number of
 !        lat/lon points
@@ -237,13 +192,6 @@ module module_nwp
          allocate(this%nwp_sst(this%xlon,this%xlat,this%xtime))
          allocate(this%nwp_ice(this%xlon,this%xlat,this%xtime))
          allocate(this%nwp_sfcT(this%xlon,this%xlat,this%xtime))
-         allocate(this%nwp_sltyp(this%xlon,this%xlat,this%xtime))
-         allocate(this%nwp_vgtyp(this%xlon,this%xlat,this%xtime))
-         allocate(this%nwp_land(this%xlon,this%xlat,this%xtime))
-         allocate(this%nwp_lu(this%xlon,this%xlat,this%xtime))
-         allocate(this%nwp_snow(this%xlon,this%xlat,this%xtime))
-         allocate(this%nwp_snowh(this%xlon,this%xlat,this%xtime))
-         allocate(this%nwp_snowc(this%xlon,this%xlat,this%xtime))
 
 !        Get variables from the data file, but only if the variable is
 !        defined for that data type.
@@ -268,41 +216,6 @@ module module_nwp
                                 this%xlat,this%xtime,this%nwp_sfcT)
             sfcT = this%nwp_sfcT(:,:,time_to_get)
          end if
-         if (this%i_sltyp .gt. 0) then
-            call ncdata%get_var(this%varnames(this%i_sltyp),this%xlon,  &
-                                this%xlat,this%xtime,this%nwp_sltyp)
-            sltyp = this%nwp_sltyp(:,:,time_to_get)
-         end if
-         if (this%i_vgtyp .gt. 0) then
-            call ncdata%get_var(this%varnames(this%i_vgtyp),this%xlon,  &
-                                this%xlat,this%xtime,this%nwp_vgtyp)
-            vgtyp = this%nwp_vgtyp(:,:,time_to_get)
-         end if
-         if (this%i_land .gt. 0) then
-            call ncdata%get_var(this%varnames(this%i_land),this%xlon,  &
-                                this%xlat,this%xtime,this%nwp_land)
-            land = this%nwp_land(:,:,time_to_get)
-         end if
-         if (this%i_lu .gt. 0) then
-            call ncdata%get_var(this%varnames(this%i_lu),this%xlon,  &
-                                this%xlat,this%xtime,this%nwp_lu)
-            lu = this%nwp_lu(:,:,time_to_get)
-         end if
-         if (this%i_snow .gt. 0) then
-            call ncdata%get_var(this%varnames(this%i_snow),this%xlon,  &
-                                this%xlat,this%xtime,this%nwp_snow)
-            snow = this%nwp_snow(:,:,time_to_get)
-         end if
-         if (this%i_snowh .gt. 0) then
-            call ncdata%get_var(this%varnames(this%i_snowh),this%xlon,  &
-                                this%xlat,this%xtime,this%nwp_snowh)
-            snowh = this%nwp_snowh(:,:,time_to_get)
-         end if
-         if (this%i_snowc .gt. 0) then
-            call ncdata%get_var(this%varnames(this%i_snowc),this%xlon,  &
-                                this%xlat,this%xtime,this%nwp_snowc)
-            snowc = this%nwp_snowc(:,:,time_to_get)
-         end if
 
 !        Close the netCDF file.
 
@@ -312,57 +225,6 @@ module module_nwp
          write(*,*) ' '
 
       end subroutine read_nwp
-
-      subroutine read_nwp_3d(this,filename,itype,numlon,numlat,numvert,numtimes,time_to_get,soilmoisture)
-
-!        This subroutine initializes arrays to receive the NWP data,
-!        and opens the file and gets the data.
-
-         class(fcst_nwp) :: this
-
-         character(len=5), intent(in) :: itype
-         character(len=*), intent(in) :: filename
-
-         integer, intent(in) :: time_to_get
-         integer, intent(inout) :: numlon, numlat, numvert, numtimes
-         real(r_single), intent(inout) :: soilmoisture(:,:,:)
-
-!        Open the file using module_ncio.f90 code, and find the number of
-!        lat/lon points
-
-         call ncdata%open(trim(filename),'r',200)
-         call ncdata%get_dim(this%dimnameEW,this%xlon)
-         call ncdata%get_dim(this%dimnameNS,this%xlat)
-         call ncdata%get_dim(this%dimnameTIME,this%xtime)
-         call ncdata%get_dim(this%dimnameVERT,this%xvert)
-
-         write(*,*) 'number of longitudes for file ', filename, this%xlon
-         numlon = this%xlon
-         write(*,*) 'number of latitudes for file ', filename, this%xlat
-         numlat = this%xlat
-         write(*,*) 'number of times for file ', filename, this%xtime
-         numtimes = this%xtime
-         write(*,*) 'number of soil levels for file ', filename, this%xvert
-         numvert = this%xvert
-
-!        Allocate all the arrays to receive data
-
-         allocate(this%nwp_soilmoisture(this%xlon,this%xlat,this%xvert,this%xtime))
-
-!        Get variable from the data file
-
-         call ncdata%get_var(this%varname3D,this%xlon,this%xlat,this%xvert,this%xtime,  &
-                                this%nwp_soilmoisture)
-         soilmoisture = this%nwp_soilmoisture(:,:,:,time_to_get)
-
-!        Close the netCDF file.
-
-         call ncdata%close
-
-         write(*,*) 'Finished read_nwp_3d'
-         write(*,*) ' '
-
-      end subroutine read_nwp_3d
 
       subroutine finish_nwp(this)
 
@@ -376,19 +238,10 @@ module module_nwp
          deallocate(this%dimnameEW)
          deallocate(this%dimnameNS)
          deallocate(this%dimnameTIME)
-         deallocate(this%dimnameVERT)
          deallocate(this%nwp_mask)
          deallocate(this%nwp_sst)
          deallocate(this%nwp_ice)
          deallocate(this%nwp_sfcT)
-         deallocate(this%nwp_sltyp)
-         deallocate(this%nwp_vgtyp)
-         deallocate(this%nwp_land)
-         deallocate(this%nwp_lu)
-         deallocate(this%nwp_snow)
-         deallocate(this%nwp_snowh)
-         deallocate(this%nwp_snowc)
-!         deallocate(this%nwp_soilmoisture)
 
          thisobs => this%head
          if(.NOT.associated(thisobs)) then
--- data_process/fvcom/process_FVCOM.f90
+++ data_process/fvcom/process_FVCOM.f90
@@ -31,10 +31,10 @@ program process_FVCOM
    character*2 :: workPath
    character*1 :: char1
 
-   integer :: MAP_PROJ, NLON, NLAT, NLEV
-   integer :: hrrrlon, hrrrlat, hrrrtimes, hrrrlevs
+   integer :: MAP_PROJ, NLON, NLAT
+   integer :: hrrrlon, hrrrlat, hrrrtimes
    integer :: fvlon, fvlat, fvtimes
-   integer :: i, j, k, t1, t2
+   integer :: i, j, t1, t2
 
    real :: rad2deg = 180.0/3.1415926
    real :: userDX, userDY, CEN_LAT, CEN_LON
@@ -43,24 +43,15 @@ program process_FVCOM
    real :: knowni, knownj, dx
    real :: one, pi, deg2rad
 
-!  For FVCOM and HRRR data
+!  For lightning data
 
    character*180 :: hrrrfile
    character*180 :: fvcomfile
 
    real(r_single), allocatable :: hrrrice(:,:), hrrrsst(:,:)
    real(r_single), allocatable :: hrrrsfcT(:,:), hrrrmask(:,:)
-   real(r_single), allocatable :: hrrrland(:,:), hrrrlu(:,:)
-   real(r_single), allocatable :: hrrrsnow(:,:), hrrrsnowh(:,:), hrrrsnowc(:,:)
-   integer, allocatable :: hrrrsltyp(:,:), hrrrvgtyp(:,:)
-   real(r_single), allocatable :: soilm(:,:,:)
    real(r_single), allocatable :: fvice(:,:), fvsst(:,:)
    real(r_single), allocatable :: fvsfcT(:,:), fvmask(:,:)
-   real(r_single), allocatable :: fvland(:,:), fvlu(:,:)
-   real(r_single), allocatable :: fvsnow(:,:), fvsnowh(:,:), fvsnowc(:,:)
-   integer, allocatable :: fvsltyp(:,:), fvvgtyp(:,:)
-   real(r_single) :: xice_threshold
-   integer :: fractional_seaice
 
 !  Declare namelists
 !  SETUP (general control namelist) :
@@ -72,17 +63,6 @@ program process_FVCOM
    read(5,setup)
    write(*,*) 'From namelist: update type is ', update_type
    write(*,*) 'From namelist: t2 is ', t2
-
-!  Set up sea ice threshold
-
-   fractional_seaice = 1
-   if (fractional_seaice == 0) then
-      xice_threshold = 0.5
-      write(*,*) 'Do not use fractional sea ice'
-   else if (fractional_seaice == 1) then
-      xice_threshold = 0.02
-      write(*,*) 'Use fraction sea ice'
-   endif
 
 !  Set geogrid fle name and obtain grid parameters
 
@@ -109,34 +89,17 @@ program process_FVCOM
    write(*,*) 'Finished reading geofile.'
    write(*,*) ' '
 
-   nlev = 9
-
 !  Allocate variables for I/O
 
    allocate(hrrrice(nlon,nlat))
    allocate(hrrrsfcT(nlon,nlat))
    allocate(hrrrsst(nlon,nlat))
    allocate(hrrrmask(nlon,nlat))
-   allocate(hrrrsltyp(nlon,nlat))
-   allocate(hrrrvgtyp(nlon,nlat))
-   allocate(hrrrland(nlon,nlat))
-   allocate(hrrrlu(nlon,nlat))
-   allocate(hrrrsnow(nlon,nlat))
-   allocate(hrrrsnowc(nlon,nlat))
-   allocate(hrrrsnowh(nlon,nlat))
-   allocate(soilm(nlon,nlat,nlev))
 
    allocate(fvice(nlon,nlat))
    allocate(fvsfcT(nlon,nlat))
    allocate(fvsst(nlon,nlat))
    allocate(fvmask(nlon,nlat))
-   allocate(fvsltyp(nlon,nlat))
-   allocate(fvvgtyp(nlon,nlat))
-   allocate(fvland(nlon,nlat))
-   allocate(fvlu(nlon,nlat))
-   allocate(fvsnow(nlon,nlat))
-   allocate(fvsnowc(nlon,nlat))
-   allocate(fvsnowh(nlon,nlat))
 
 !  Read HRRR input datasets
 
@@ -145,8 +108,7 @@ program process_FVCOM
 
    call fcst%initial(' HRRR')
    call fcst%list_initial
-   call fcst%read_n(trim(hrrrfile),'HRRR',hrrrlon,hrrrlat,hrrrtimes,t1,hrrrmask,hrrrsst,hrrrice,hrrrsfcT,hrrrsltyp,hrrrvgtyp,hrrrland,hrrrlu,hrrrsnow,hrrrsnowh,hrrrsnowc)
-   call fcst%read_n3(trim(hrrrfile),'HRRR',hrrrlon,hrrrlat,hrrrlevs,hrrrtimes,t1,soilm)
+   call fcst%read_n(trim(hrrrfile),'HRRR',hrrrlon,hrrrlat,hrrrtimes,t1,hrrrmask,hrrrsst,hrrrice,hrrrsfcT)
    call fcst%finish
 
 !  Check that the dimensions match
@@ -160,7 +122,6 @@ program process_FVCOM
       stop 123
    endif
 
-   write(*,*) 'hrrrlevs: ', hrrrlevs
    write(*,*) 'hrrrtimes: ', hrrrtimes
    write(*,*) 'time to use: ', t1
 
@@ -170,7 +131,7 @@ program process_FVCOM
 
    call fcst%initial('FVCOM')
    call fcst%list_initial
-   call fcst%read_n(trim(fvcomfile),'FVCOM',fvlon,fvlat,fvtimes,t2,fvmask,fvsst,fvice,fvsfcT,fvsltyp,fvvgtyp,fvland,fvlu,fvsnow,fvsnowh,fvsnowc)
+   call fcst%read_n(trim(fvcomfile),'FVCOM',fvlon,fvlat,fvtimes,t2,fvmask,fvsst,fvice,fvsfcT)
    call fcst%finish
 
 !  Check that the dimensions match
@@ -188,38 +149,13 @@ program process_FVCOM
    write(*,*) 'time to use: ', t2
 
 !  Update with FVCOM fields
-!  Use FVCOM values for SST, TSK, and SEAICE
-!  Ensure consistency for LU_INDEX, XLAND, IVGTYP, ISLTYP, LANDMASK
 
    do j=1,nlat
       do i=1,nlon
-         if (fvmask(i,j) .gt. 0.) then
-            if (fvsst(i,j) .ge. -80.0) then
-               hrrrice(i,j) = fvice(i,j)
-               hrrrsst(i,j) = fvsst(i,j) + 273.15
-               hrrrsfcT(i,j) = fvsst(i,j) + 273.15
-            endif
-            do k=1,hrrrlevs
-               soilm(i,j,k) = 1.0
-            enddo
-            if (fvice(i,j) .lt. xice_threshold) then
-               hrrrsltyp(i,j) = 14
-               hrrrvgtyp(i,j) = 17
-               hrrrmask(i,j) = 0.0
-               hrrrlu(i,j) = 17.0
-               hrrrland(i,j) = 2.0
-            else
-               hrrrsltyp(i,j) = 16
-               hrrrvgtyp(i,j) = 15
-               hrrrmask(i,j) = 1.0
-               hrrrlu(i,j) = 15.0
-               hrrrland(i,j) = 1.0
-            endif
-            if (fvice(i,j) < 0.001 .and. hrrrsnow(i,j) > 0.0) then
-               hrrrsnow(i,j) = 0.0
-               hrrrsnowh(i,j) = 0.0
-               hrrrsnowc(i,j) = 0.0
-            endif
+         if (fvmask(i,j) > 0. .and. fvsst(i,j) .ge. -100.0) then
+            hrrrice(i,j) = fvice(i,j)
+            hrrrsst(i,j) = fvsst(i,j) + 273.15
+            hrrrsfcT(i,j) = fvsst(i,j) + 273.15
          endif
       enddo
    enddo
@@ -230,19 +166,9 @@ program process_FVCOM
    if (update_type .eq. 1) then
       call geo%replace_var("SST",NLON,NLAT,hrrrsst)
       call geo%replace_var("TSK",NLON,NLAT,hrrrsfcT)
-      call geo%replace_var("LANDMASK",NLON,NLAT,hrrrmask)
    else
       call geo%replace_var("SEAICE",NLON,NLAT,hrrrice)
-      call geo%replace_var("ISLTYP",NLON,NLAT,hrrrsltyp)
-      call geo%replace_var("IVGTYP",NLON,NLAT,hrrrvgtyp)
-      call geo%replace_var("LANDMASK",NLON,NLAT,hrrrmask)
-      call geo%replace_var("XLAND",NLON,NLAT,hrrrland)
-      call geo%replace_var("LU_INDEX",NLON,NLAT,hrrrlu)
-      call geo%replace_var("SNOW",NLON,NLAT,hrrrsnow)
-      call geo%replace_var("SNOWH",NLON,NLAT,hrrrsnowh)
-      call geo%replace_var("SNOWC",NLON,NLAT,hrrrsnowc)
-      call geo%replace_var("SMOIS",NLON,NLAT,hrrrlevs,soilm)
    endif
    call geo%close
 
-end program process_FVCOM
+end program process_FVCOM 
